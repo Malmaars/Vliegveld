e5d3fe14a3ec013499a20a81b73fb039a77f48db
Smos Bois <simondeb1505@gmail.com>
2020-05-01 22:38:43 +0200

    Camera = groter
    Alles wat groter gemaakt

--- Vliegveld/Assets/Scripts/Spawner.cs
+++ Vliegveld/Assets/Scripts/Spawner.cs
@@ -6,6 +6,7 @@ public class Spawner : MonoBehaviour
 {
     public GameObject[] Itemlist;
     public GameObject[] Bags;
+    private GameObject currentBag;
     System.Random rnd;
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,8 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(currentBag != null)
+        currentBag.transform.position = new Vector3(currentBag.transform.position.x, Mathf.Lerp(currentBag.transform.position.y, 0, 0.02f), currentBag.transform.position.z);
     }
 
     private void OnMouseDown()
@@ -27,13 +29,15 @@ public class Spawner : MonoBehaviour
     public void SpawnItems()
     {
         int boxnmbr = rnd.Next(0, 3);
-        GameObject Bag = Instantiate(Bags[boxnmbr], new Vector3(0, 8, 0), new Quaternion(0, 0, 0, 0));
-        for(int i = 0; i < 10; i++)
+        GameObject Bag = Instantiate(Bags[boxnmbr], new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
+        for(int i = 0; i <= 5; i++)
         {
             int objectnmbr = rnd.Next(0, 3);
             Debug.Log(objectnmbr);
-            GameObject temp = Instantiate(Itemlist[objectnmbr], new Vector3(Random.Range(-2f,2f), Random.Range(-1f, 1f), 0), new Quaternion(0,0,0,0), Bag.transform);
+            GameObject temp = Instantiate(Itemlist[objectnmbr], new Vector3(Random.Range(-20f,20f), Random.Range(-10f, 10f), 0), new Quaternion(0,0,0,0), Bag.transform);
         }
+        Bag.transform.position = new Vector3(0, 120, 0);
+        currentBag = Bag;
         
     }
 }
